a7987ea3a26fe662fddf260a9b0d84e813afd29c
imnaeem <ceinfo.pk@gmail.com>
2025-03-24 01:43:44 +0500

    chore: add metadata to portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,12 @@
 import PortfolioList from '@/components/portfolio/PortfolioList';
 import Title from '@/components/shared/Title';
 import { Fade } from '@mui/material';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Portfolio | Muhammad Naeem',
+	description: 'Checkout my portfolio of projects built with React, Next.js, Node.js, and GraphQL.',
+};
 
 const Portfolio = () => {
 	return (
